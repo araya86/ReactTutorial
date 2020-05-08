2c6dea0b4969d43e918e77a529f90dc3e307b633
araya86 <araya.wirote@gmail.com>
2020-05-08 15:19:02 +0700

    updated to remote github

--- src/Components/Store/index.js
+++ src/Components/Store/index.js
@@ -6,7 +6,11 @@ export default class Catalog extends Component {
         console.log(this.props.shop)
         return (
             <div>
-                
+                {/* <h2>{id}</h2> */}
+                <img src={img} width="800px" height="800px"/>
+                <h3>{name}</h3>
+                <h4>{detail}</h4>
+                <h5>{adress}</h5>
             </div>
         );
     }
--- src/data.js
+++ src/data.js
@@ -2,23 +2,23 @@ export const shopdata = [
 
     {
         id: 1,
-        name: "ชื่อร้าน",
-        img: "รูป1",
-        detail: "เรายระเอียด",
-        Adress: "ที่อยู่"
+        name: "Just a Little",
+        img: "https://scontent.fnak2-1.fna.fbcdn.net/v/t1.0-9/26047121_1628986067140735_9116690955254680745_n.jpg?_nc_cat=101&_nc_sid=85a577&_nc_eui2=AeGXkjVClXfinX1vJpJ2GauugYW6UIIDARKBhbpQggMBElDkvGYU0nlOg8vbmrAikUk&_nc_ohc=bekIc2l6xMgAX8Ra0qF&_nc_ht=scontent.fnak2-1.fna&oh=3282899fdde0d32362b209f0c8e33f6e&oe=5EDC6CAF",
+        detail: "รายระเอียด",
+        adress: "ที่อยู่"
         },
     {
         id: 2,
-        name: "ชื่อร้าน2",
-        img: "รูป2",
-        detail: "เรายระเอียด2",
-        Adress: "ที่อยู่2"
+        name: "LIGHT HOUSE",
+        img: "https://scontent.fnak2-1.fna.fbcdn.net/v/t1.0-9/17022355_1409335952451648_1116382878617534823_n.jpg?_nc_cat=105&_nc_sid=85a577&_nc_eui2=AeEqZKO614QBQTaPpKClB6bsLOsF8oncLzws6wXyidwvPLTx_IkTOf-vwCI02aWfbNc&_nc_ohc=jsuE52HJUroAX8yQskD&_nc_ht=scontent.fnak2-1.fna&oh=366de4be1538efa38ad37316d03fe574&oe=5EDC40CC",
+        detail: "รายระเอียด",
+        adress: "ที่อยู่"
         },
     {
         id: 3,
-        name: "ชื่อร้าน3",
-        img: "รูป3",
-        detail: "เรายระเอียด3",
-        Adress: "ที่อยู่3"
+        name: "โกนขแมร์",
+        img: "https://scontent.fnak2-1.fna.fbcdn.net/v/t1.0-9/67844787_2047361802035430_8311136621160300544_n.jpg?_nc_cat=103&_nc_sid=85a577&_nc_eui2=AeGOQkRUCSR7s4yjYtrWkNawpZrbhiKMyZSlmtuGIozJlDph02ss95u27yz_vBcGcTA&_nc_ohc=EKhEdPYmNMYAX9AriA2&_nc_ht=scontent.fnak2-1.fna&oh=c27c93bd720b3925f0ba22ad8be2efb6&oe=5EDA991F",
+        detail: "รายระเอียด",
+        adress: "ที่อยู่"
         },
 ]
