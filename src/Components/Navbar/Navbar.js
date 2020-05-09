5ff56aa714c10427ced62b5bddcccdd689e5265d
araya86 <araya.wirote@gmail.com>
2020-05-09 14:00:10 +0700

    updated to remote github

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 function Navbar () {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">
-          LOGO
+        <a className="navbar-brand ml-2" href="/home">
+        <i class="fas fa-utensils"></i>
         </a>
         <button
           className="navbar-toggler"
@@ -20,20 +20,20 @@ function Navbar () {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <ul className="navbar-nav m-auto mt-2 mt-lg-0">
             <li className="nav-item active">
-              <Link className="nav-link" to="/home">
-                Home <span className="sr-only">(current)</span>
+              <Link className="nav-link text-white text-uppercase ml-5" to="/home">
+              <i class="fas fa-home"></i> Home&nbsp; <span className="sr-only">(Current)</span>
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
+            <li className="nav-item active">
+              <Link className="nav-link text-white text-uppercase ml-5" to="/about">
                 Food-Shop
               </Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                contact
+            <li className="nav-item active">
+              <Link className="nav-link text-white text-uppercase ml-5" to="/contact">
+                Contact us
               </Link>
             </li>
           </ul>
